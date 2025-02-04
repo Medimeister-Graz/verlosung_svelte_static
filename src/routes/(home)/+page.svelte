7a89b41fcf546f8299a49ab72a3607ba3a4755be
flashedk <needfor37@gmail.com>
2025-02-04 09:50:19 +0100

    updated static site to fit new design, ready for publishing

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,62 +1,72 @@
 <script lang="ts">
+	//ui components
+	import LightModeButton from '$lib/components/lightmodebutton.svelte';
+
+	import img from '$lib/img/bg.jpg';
+
 	// core components
-	import mmgraz from '$lib/img/mmgraz_transparent.png';
 	import { base } from '$app/paths';
 </script>
 
-<div class="container mx-auto px-4 min-h-screen">
-	<div class="flex content-center items-center justify-center min-h-screen">
-		<div class="w-full lg:w-6/12 px-4">
-			<div
-				class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-slate-200 border-0"
-			>
-				<div class="rounded-t mb-0 px-6 py-6 pb-4">
-					<div class="btn-wrapper text-center flex justify-center">
-						<img alt="Logo der Medimeister" src={mmgraz} width="60%" />
-					</div>
-					<div class="text-black text-center text-4xl mb-3 font-bold tracking-wide uppercase">
-						<small>Ticketportal</small>
-					</div>
-					<hr class="mt-6 border-b-1 border-slate-950" />
-				</div>
-				<div id="border" class="px-4">
-					<div
-						class="flex-auto px-2 bg-yellow-300/50 rounded-md text-center font-semibold border-x-8 border-yellow-500"
-					>
-						<h1>
-							Alle Codes wurden eingelöst. Es gibt keine weitere Verteilung über die Warteliste.
-						</h1>
-						<h1>
-							Vielen Dank an alle, die sich angemeldet haben und an die Regeln gehalten haben!
-						</h1>
-						<h1>Wir freuen uns auf euch in Obermehler!</h1>
-					</div>
-					<br />
-					<div
-						class="flex-auto px-2 bg-green-300/50 rounded-md text-center font-semibold border-x-8 border-green-500"
-					>
-						<h1>
-							Eure diesjährigen Daten wurden archiviert und fürs nächste Jahr gespeichert. Im
-							nächsten Jahr könnt ihr diese direkt wieder verwenden, nachdem ihr eure E-Mail und
-							Studienjahr aktualisiert habt.
-						</h1>
-						<h1>Die Infos zur Verlosung findet ihr dann nächstes Jahr wieder auf Instagram.</h1>
-					</div>
-				</div>
-
-				<div class="justify-center px-4 lg:px-10 pt-0">
-					<div class="text-center">
-						<a href="mailto:kontakt@medimeistergraz.org" class="text-slate-600 text-sm"
-							>Fragen oder Probleme?</a
-						>
-					</div>
-					<div class="pb-4 text-center">
-						<a href="{base}/dsgvo" class="text-slate-400 text-xs">Datenschutzerklärung</a>
-						|
-						<a href="{base}/impressum" class="text-slate-400 text-xs mr-14">Impressum</a>
-					</div>
+<div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-screen">
+	<div class="flex items-center justify-center py-12 min-h-screen">
+		<div class="mx-auto grid w-[350px] gap-6">
+			<div class="relative flex items-center text-lg font-medium">
+				<!-- <Command class="mr-2 h-6 w-6" /> -->
+				Medimeisterschaften Graz
+				<div class="relative flex items-center justify-center ml-auto">
+					<LightModeButton />
 				</div>
 			</div>
+
+			<div class="grid gap-2 text-center">
+				<h1 class="text-3xl font-bold">Ticketportal</h1>
+
+				<div
+					class="flex-auto px-2 bg-yellow-300/50 rounded-md text-center border-x-8 border-yellow-500"
+				>
+					<h1>
+						Alle Codes wurden eingelöst. Es gibt keine weitere Verteilung über die Warteliste.
+					</h1>
+					<h1>Vielen Dank an alle, die sich angemeldet haben und an die Regeln gehalten haben!</h1>
+					<h1>Wir freuen uns auf euch in Obermehler!</h1>
+				</div>
+
+				<div
+					class="flex-auto px-2 bg-green-300/50 rounded-md text-center border-x-8 border-green-500"
+				>
+					<h1>
+						Eure diesjährigen Daten wurden archiviert und fürs nächste Jahr gespeichert. Im nächsten
+						Jahr könnt ihr diese direkt wieder verwenden, nachdem ihr eure E-Mail und Studienjahr
+						aktualisiert habt.
+					</h1>
+					<h1>Die Infos zur Verlosung findet ihr dann nächstes Jahr wieder auf Instagram.</h1>
+				</div>
+			</div>
+
+			<div class=" text-center text-sm">
+				<a href="mailto:kontakt@medimeistergraz.org" class="underline">Fragen oder Probleme?</a>
+			</div>
+
+			<p class="text-muted-foreground px-8 text-center text-sm">
+				Durch klicken des "Login"-Buttons, stimmst du unserer
+				<a href="{base}/dsgvo" class="hover:text-primary underline underline-offset-4">
+					Datenschutzvereinbarung
+				</a>
+				zu.
+				<a href="{base}/impressum" class="hover:text-primary underline underline-offset-4">
+					Impressum
+				</a>
+			</p>
 		</div>
+	</div>
+	<div class="bg-muted hidden lg:block">
+		<img
+			src={img}
+			alt="background"
+			width="1920"
+			height="1080"
+			class="h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import registerBg from '$lib/img/bg.jpg';
+
+	//dark mode toggle
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
-<div>
-	<main>
-		<section class="relative w-full h-full py-0 min-h-screen">
-			<!--<div class="absolute top-0 w-full h-full bg-green-600/50 bg-no-repeat bg-full z-0" />-->
-			<div
-				class="bg-cover absolute h-full w-full blur-sm z-0"
-				style="background-image: url({registerBg})"
-			/>
-			<div class="min-h-screen">
-				<slot />
-			</div>
-		</section>
-	</main>
-</div>
+<main>
+	<ModeWatcher />
+
+	<slot />
+</main>
